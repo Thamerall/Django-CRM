63ce963d6c157c4687d5deb17725dfb271f725cd
Thamer Allahabi <thamer.a.allahabi@gmail.com>
2019-07-13 08:40:56 -0400

    Update views.py

--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Customer
 from .serializers import *
